584d01113d5959ad1f57e9a72e319a0322798a75
Andreis Purim <andreispurim@gmail.com>
2020-09-29 16:22:56 -0300

    Added gh-pages dep.

---
+++ build/precache-manifest.31b8fea5e4f982c52cb2d07f87efb642.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "350fd4a4ffff94b62541415f499fb9fe",
+    "url": "/tem-pupo-hoje/index.html"
+  },
+  {
+    "revision": "d1062c77816670ad7070",
+    "url": "/tem-pupo-hoje/static/js/2.29b1fb5a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/tem-pupo-hoje/static/js/2.29b1fb5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "269e64ff673cf41554b5",
+    "url": "/tem-pupo-hoje/static/js/main.63770800.chunk.js"
+  },
+  {
+    "revision": "8282e3b8ca6f85f15cc1",
+    "url": "/tem-pupo-hoje/static/js/runtime-main.78766f06.js"
+  }
+]);
